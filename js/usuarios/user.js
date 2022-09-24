fec8ae7f6622a2cec2a037397157ffe25a2881b6
unknown <andersonromeroloarte@gmail.com>
2022-09-24 03:02:02 -0500

    initial commit

--- js/usuarios/user.js
+++ js/usuarios/user.js
@@ -27,4 +27,41 @@ function agregarUsuario() {
   });
 }
 
+$(".a-eliminar").click(function () {
+  let idUser = this.id;
+  Swal.fire({
+    title: '¿Seguro que quiere eliminar al Usuario?',
+    showDenyButton: true,
+    confirmButtonText: 'Eliminar',
+    denyButtonText: `No Eliminar`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      eliminarUsuario(idUser,"controlador/user.controller.php");
 
+    } else if (result.isDenied) {
+      Swal.fire('Usuario no Elimado', '', 'info')
+    }
+  })
+
+  
+  
+});
+
+
+async function eliminarUsuario(id, url) {
+  try {
+
+    let datos = new FormData();
+    datos.append("delete", "1");
+    datos.append("idUsuario", id);
+    let response = await fetch(url, {
+      method: "POST",
+      body: datos,
+    });
+ 
+    location.replace("usuarios");
+  } catch (e) {
+    console.log(e);
+  }
+}
--- usuarios.php
+++ usuarios.php
@@ -54,7 +54,7 @@ include "includes/header.php";
                                         </svg>
                                     </a>
 
-                                    <a class="a-eliminar" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-x align-middle me-2">
+                                    <a class="a-eliminar" id="<?= $user["id"]; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-x align-middle me-2">
                                             <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
                                             <circle cx="8.5" cy="7" r="4"></circle>
                                             <line x1="18" y1="8" x2="23" y2="13"></line>
